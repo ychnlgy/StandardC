0f2af73de0b2169b2a36ff8dcc800605679a6cdc
Yuchen Li <ychnlgy@gmail.com>
2018-04-04 21:36:01 -0700

    Added large tests for Socket

--- stdc/net/Socket/TCPSocket/TCPSocket_test.c
+++ stdc/net/Socket/TCPSocket/TCPSocket_test.c
@@ -1,6 +1,7 @@
 #include "stdc/unittest.h"
 
 #include <string.h>
+#include <unistd.h>
 #include <pthread.h>
 pthread_mutex_t mutex;
 pthread_cond_t cond;
@@ -12,10 +13,12 @@ MemoryObject* mem;
 TCPSocketObject* serverSock;
 TCPSocketObject* clientSock;
 
-CStr serverHello = "Hello";
+CStr serverHello = "0123456789";
 CStr clientGoodbye = "Goodbye (from Grook)";
 CStr LOCAL_IP = "127.0.0.1";
 int PORT = 8080;
+
+// One message
 
 Ptr serverOneMsg(Ptr args) {
     pthread_mutex_lock(&mutex);
@@ -65,6 +68,155 @@ Ptr clientOneMsg(Ptr args) {
     return NULL;
 }
 
+// Multiple messages
+
+long MULTIPLE = 100;
+CStr HELLO_WORLD = "Hello world!";
+CStr FOO_BAZ_BAR = "Foo Baz Bar";
+
+Ptr serverMultipleMsgs(Ptr args) {
+    pthread_mutex_lock(&mutex);
+    _TestCase* _testCase = args;
+
+    ASSERT(TCPSocket.bindany(serverSock, PORT));
+    ASSERT(TCPSocket.listen(serverSock, 1));
+    
+    started = true;
+    pthread_cond_signal(&cond);
+    pthread_cond_wait(&cond, &mutex);
+    
+    TCPSocketObject* connectedSock = TCPSocket.accept(serverSock, mem);
+    
+    long i;
+    for (i=0; i<MULTIPLE; i++) {
+        ASSERT(strlen(HELLO_WORLD) == TCPSocket.write(connectedSock, HELLO_WORLD));
+        
+        pthread_cond_signal(&cond);
+        pthread_cond_wait(&cond, &mutex);
+        
+        FileData* fd;
+        while((fd = TCPSocket.read(connectedSock, mem)) == NULL) {
+            sleep(1);
+        }
+        ASSERT(fd->n == strlen(FOO_BAZ_BAR));
+        ASSERT(strcmp(fd->d, FOO_BAZ_BAR) == 0);
+    }
+
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+Ptr clientMultipleMsgs(Ptr args) {
+    pthread_mutex_lock(&mutex);
+    _TestCase* _testCase = args;
+    
+    if (!started)
+        pthread_cond_wait(&cond, &mutex);
+    
+    ASSERT(TCPSocket.connect(clientSock, LOCAL_IP, PORT));
+    
+    int i = 0;
+    for (i=0; i<MULTIPLE; i++) {
+        pthread_cond_signal(&cond);
+        pthread_cond_wait(&cond, &mutex);
+        
+        FileData* fd;
+        while((fd = TCPSocket.read(clientSock, mem)) == NULL) {
+            sleep(1);
+        }
+        ASSERT(fd->n == strlen(HELLO_WORLD));
+        ASSERT(strcmp(fd->d, HELLO_WORLD) == 0);
+        ASSERT(strlen(FOO_BAZ_BAR) == TCPSocket.write(clientSock, FOO_BAZ_BAR));
+    }
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+// Large data + writestr
+
+StringObject* multiplyString(CStr cstr, int n, MemoryObject* mem) {
+    MemoryObject* scope = Memory.new();
+    StringObject* s = Memory.make(scope, String.new);
+    String.set(s, cstr);
+    ListObject* list = Memory.make(scope, List.new);
+    int i;
+    for (i=0; i<n; i++)
+        List.push(list, s);
+    StringObject* out = String.merge(list, mem);
+    decref(scope);
+    return out;
+}
+
+int SERVER_REPEAT = 1000;
+int CLIENT_REPEAT = 1000;
+CStr SERVER_LARGE = "Server";
+CStr CLIENT_LARGE = "Client!";
+
+Ptr serverLargeMsg(Ptr args) {
+    pthread_mutex_lock(&mutex);
+    _TestCase* _testCase = args;
+
+    StringObject* serverMsg = multiplyString(SERVER_LARGE, SERVER_REPEAT, mem);
+
+    ASSERT(TCPSocket.bindany(serverSock, PORT));
+    ASSERT(TCPSocket.listen(serverSock, 1));
+    
+    started = true;
+    pthread_cond_signal(&cond);
+    pthread_cond_wait(&cond, &mutex);
+    
+    TCPSocketObject* connectedSock = TCPSocket.accept(serverSock, mem);
+    ASSERT(String.size(serverMsg) == TCPSocket.writestr(connectedSock, serverMsg));
+    
+    pthread_cond_signal(&cond);
+    pthread_cond_wait(&cond, &mutex);
+    
+    FileData* fd;
+    while((fd = TCPSocket.read(connectedSock, mem)) == NULL) {
+        sleep(1);
+    }
+    ASSERT(fd->n == CLIENT_REPEAT*strlen(CLIENT_LARGE));
+    
+    int i;
+    for (i=0; i<fd->n; i++)
+        ASSERT(fd->d[i] == CLIENT_LARGE[MOD(i, strlen(CLIENT_LARGE))]);
+
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+Ptr clientLargeMsg(Ptr args) {
+    pthread_mutex_lock(&mutex);
+    _TestCase* _testCase = args;
+    
+    StringObject* clientMsg = multiplyString(CLIENT_LARGE, CLIENT_REPEAT, mem);
+    
+    if (!started)
+        pthread_cond_wait(&cond, &mutex);
+    
+    ASSERT(TCPSocket.connect(clientSock, LOCAL_IP, PORT));
+    
+    pthread_cond_signal(&cond);
+    pthread_cond_wait(&cond, &mutex);
+    
+    FileData* fd;
+    while((fd = TCPSocket.read(clientSock, mem)) == NULL) {
+        sleep(1);
+    }
+    ASSERT(fd->n == strlen(SERVER_LARGE)*SERVER_REPEAT);
+    
+    int i;
+    for (i=0; i<fd->n; i++)
+        ASSERT(fd->d[i] == SERVER_LARGE[MOD(i, strlen(SERVER_LARGE))]);
+    
+    ASSERT(String.size(clientMsg) == TCPSocket.writestr(clientSock, clientMsg));
+    pthread_cond_signal(&cond);
+    
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
 void runServerAndClient(ThreadFunc server, ThreadFunc client, Ptr args) {
     pthread_create(&serverThread, NULL, server, args);
     pthread_create(&clientThread, NULL, client, args);
@@ -96,5 +248,13 @@ RUN
     CASE("interaction-cstr")
         runServerAndClient(&serverOneMsg, &clientOneMsg, _testCase);
     END
+    
+    CASE("multiple interactions")
+        runServerAndClient(&serverMultipleMsgs, &clientMultipleMsgs, _testCase);
+    END
+/*    */
+/*    CASE("large data")*/
+/*        runServerAndClient(&serverLargeMsg, &clientLargeMsg, _testCase);*/
+/*    END*/
 
 STOP
